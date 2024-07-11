79c88cea87675a3606ea0636ffd302b595e07c17
GideonCove <gideoncove@gmail.com>
2024-07-11 12:28:23 -0400

    init commit

---
+++ tunr_project/tunr/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+# Everytime you change a model, run  python3 manage.py makemigrations, then python3 manage.py migrate  
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    title = models.CharField(max_length= 100, default = "no title")
+    album = models.CharField(max_length= 100, default="no album")
+    preview_url = models.CharField(max_length= 200, null=True)
+
+    def __str__(self):
+        return self.title
